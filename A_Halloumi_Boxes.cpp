87cf1182202d5a16b507ff139ec1783fb5a97a57
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-02-23 11:22:00 +0500

    correct Halloumi Boxes problem

--- A_Halloumi_Boxes.cpp
+++ A_Halloumi_Boxes.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(vector <int> v){
+bool check(vector<int> v){
     int n = v.size();
-    for(int i = 0; i<n ; i++){
+    for(int i=0; i<n-1; i++){
         if(v[i] > v[i+1]) return false;
     }
     return true;
 }
+
+
 int main(){
-    int t; cin>>t;
-    for( int i=0 ;i<t; i++){
-        int n; cin>>n;
-        int k; cin>>k;
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        int n, k;
+        cin >> n >> k;
         vector<int> v(n);
-        for(int i = 0; i<n; i++){
-            cin>>v[i];
+        for( int j = 0; j < n; j++){
+            cin >> v[j];
         }
-        if(k>=2) cout<<"YES"<<endl;
-        else if (k==1){
-            if(check(v)) cout<<"YES"<<endl;
-            else cout<<"NO"<<endl;
+        if(k>=2){
+            cout << "YES" << endl;
+        }else if(k==1){
+            if(check(v)){
+                cout << "YES" << endl;
+            }else{
+                cout << "NO" << endl;
+            }
         }
     }
     return 0;
