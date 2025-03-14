99dcbec2dfaddbbc7213039d67e7b9375dc97862
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-03-14 21:43:48 +0500

    Capturing phase

--- jsinterview/interview.js
+++ jsinterview/interview.js
@@ -9,3 +9,25 @@
 // // Output 
 // undefined
 // 200
+
+// event propagations : 
+// In JavaScript, event propagation refers to how events travel through the DOM (Document Object Model) when an event (like a click or keypress) occurs.
+
+// There are two phases of event propagation:
+
+// Capturing Phase: The event travels from the root of the DOM tree down to the target element.
+
+// Bubbling Phase: The event travels from the target element back up to the root of the DOM tree.
+
+// You can control event propagation using methods like:
+
+// event.stopPropagation(): Stops the event from propagating further.
+
+// event.stopImmediatePropagation(): Stops the event and prevents other listeners on the same element from being called.
+// 
+
+// Bubbling Phase: The event travels from the target element back up to the root of the DOM tree.
+
+// You can control event propagation using methods like:
+
+// event.stopPropagation(): Stops the event from propagating further.
