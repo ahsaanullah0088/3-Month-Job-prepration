d11c2644da2a6db61405c3d13ebc1be930274c21
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-03-24 01:48:37 +0500

    implement react basic flow of react

---
+++ React/project01/project1/readme.md
@@ -0,0 +1,8 @@
+React Core library hai 
+
+niche is k aajati hain 
+React-DOM  for websites , React-Native for apps 
+
+// components k funcitons ko big letters mein hone chahiey first elemenet big letterhona chhaiye 
+// 
+ 
---
+++ React/project01/project1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 bg-blue-200'>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world!
+      </h1>
+      <p className='text-2xl font-bold underline'>
+        Hello world!
+      </p>
+      
+    </div>
+  )
+}
+
+export default App
