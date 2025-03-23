f4942efc73045ec6454860d85e41be66dbaea0d7
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-03-24 03:01:14 +0500

    props in react

--- React/project01/project1/src/App.jsx
+++ React/project01/project1/src/App.jsx
@@ -1,36 +1,54 @@
-import React , {useState} from 'react'
+// import React , {useState} from 'react'
 
+// const App = () => {
+//  let [counter, setCounter] = useState(15);
+//   // let counter = 15;
+//   const addValue = () =>{
+//     const addcounter = counter + 1;
+//     if(addcounter <= 20) {
+//       setCounter(addcounter);
+//       return;
+//     }
+//     alert("Counter value can't be more than 20");
+//     setCounter(addcounter);
+//     }
+//     const remvoveValue = () =>{
+//       const rmcounter = counter - 1;
+//       if(rmcounter >= 0) {
+//         setCounter(rmcounter);
+//         return;
+//       }alert("Counter value can't be less than 0");
+
+//     }
+//   return (
+//     <div>
+//       <h1>Ahsaan Ullah</h1>
+//       <h1>Counter value : {counter}</h1>
+//       <button className='border' onClick={addValue}>
+//         Add Value
+//       </button>
+//       <br />
+//       <button className='border' onClick={remvoveValue}>Remove value</button>
+//     </div>
+//   )
+// }
+
+// export default App
+let myObject = {
+  name : "Ahsaan Ullah",
+  age :21,
+  country : "Pakistan",
+}
+
+import React from 'react';
+import Card from './components/card';
 const App = () => {
- let [counter, setCounter] = useState(15);
-  // let counter = 15;
-  const addValue = () =>{
-    const addcounter = counter + 1;
-    if(addcounter <= 20) {
-      setCounter(addcounter);
-      return;
-    }
-    alert("Counter value can't be more than 20");
-    setCounter(addcounter);
-    }
-    const remvoveValue = () =>{
-      const rmcounter = counter - 1;
-      if(rmcounter >= 0) {
-        setCounter(rmcounter);
-        return;
-      }alert("Counter value can't be less than 0");
-
-    }
   return (
-    <div>
-      <h1>Ahsaan Ullah</h1>
-      <h1>Counter value : {counter}</h1>
-      <button className='border' onClick={addValue}>
-        Add Value
-      </button>
-      <br />
-      <button className='border' onClick={remvoveValue}>Remove value</button>
+    <div>     
+      <Card someobje = {myObject}/>
     </div>
   )
 }
 
 export default App
+
