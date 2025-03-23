bc5f44bc725a7349b9ec82d2a9f58891e4cdc295
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-03-24 02:27:25 +0500

    create counter in react with conditions in react

--- React/project01/project1/src/App.jsx
+++ React/project01/project1/src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 const App = () => {
+ let [counter, setCounter] = useState(15);
+  // let counter = 15;
+  const addValue = () =>{
+    const addcounter = counter + 1;
+    if(addcounter <= 20) {
+      setCounter(addcounter);
+      return;
+    }
+    alert("Counter value can't be more than 20");
+    setCounter(addcounter);
+    }
+    const remvoveValue = () =>{
+      const rmcounter = counter - 1;
+      if(rmcounter >= 0) {
+        setCounter(rmcounter);
+        return;
+      }alert("Counter value can't be less than 0");
+
+    }
   return (
-    <div className='text-red-500 bg-blue-200'>
-      <h1 className='text-3xl font-bold underline'>
-        Hello world!
-      </h1>
-      <p className='text-2xl font-bold underline'>
-        Hello world!
-      </p>
-      
+    <div>
+      <h1>Ahsaan Ullah</h1>
+      <h1>Counter value : {counter}</h1>
+      <button className='border' onClick={addValue}>
+        Add Value
+      </button>
+      <br />
+      <button className='border' onClick={remvoveValue}>Remove value</button>
     </div>
   )
 }
